cca589242ce42f0fa5ec454598dc97ee49e53d67
Ryo Utsunomiya <millionsnowliving@gmail.com>
2018-11-27 22:47:17 +0900

    resolveJsonModule

--- index.ts
+++ index.ts
@@ -1,12 +1,6 @@
-import * as fs from "fs";
 import { statement } from "./statement";
-
-function readJSONSync(filePath) {
-  return JSON.parse(fs.readFileSync(filePath, { encoding: "UTF-8" }));
-}
-
-const invoices = readJSONSync("./invoices.json");
-const plays = readJSONSync("./plays.json");
+import * as invoices from "./invoices.json";
+import * as plays from "./plays.json";
 
 invoices.forEach(invoice => {
   console.log(statement(invoice, plays));
