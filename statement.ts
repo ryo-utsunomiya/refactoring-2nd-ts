55ed2c60cea58046c7ae2c5c77c7feb2d648284e
Ryo Utsunomiya <millionsnowliving@gmail.com>
2018-11-28 21:38:58 +0900

    rename

--- statement.ts
+++ statement.ts
@@ -13,22 +13,22 @@ interface Play {
   type: string;
 }
 
-function amountFor(perf: Performance, play: Play): number {
+function amountFor(aPerformance: Performance, play: Play): number {
   let result = 0;
 
   switch (play.type) {
     case "tragedy":
       result = 40000;
-      if (perf.audience > 30) {
-        result += 1000 * (perf.audience - 30);
+      if (aPerformance.audience > 30) {
+        result += 1000 * (aPerformance.audience - 30);
       }
       break;
     case "comedy":
       result = 30000;
-      if (perf.audience > 20) {
-        result += 10000 + 500 * (perf.audience - 20);
+      if (aPerformance.audience > 20) {
+        result += 10000 + 500 * (aPerformance.audience - 20);
       }
-      result += 300 * perf.audience;
+      result += 300 * aPerformance.audience;
       break;
     default:
       throw new Error(`unknown type: ${play.type}`);
