43a1912d957952130e16c1397293d42a5ec9d07f
Ryo Utsunomiya <millionsnowliving@gmail.com>
2018-12-01 19:52:52 +0900

    temp to query

--- statement.ts
+++ statement.ts
@@ -49,17 +49,20 @@ export function statement(
     minimumFractionDigits: 2
   }).format;
 
+  function playFor(aPerformance: Performance) {
+    return plays[aPerformance.playID];
+  }
+
   for (let perf of invoice.performances) {
-    const play = plays[perf.playID];
-    let thisAmount = amountFor(perf, play);
+    let thisAmount = amountFor(perf, playFor(perf));
 
     // add volume credits
     volumeCredits += Math.max(perf.audience - 30, 0);
     // add extra credit for every ten comedy attendees
-    if ("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5);
+    if ("comedy" === playFor(perf).type) volumeCredits += Math.floor(perf.audience / 5);
 
     // print line for this order
-    result += `  ${play.name}: ${format(thisAmount / 100)} (${
+    result += `  ${playFor(perf).name}: ${format(thisAmount / 100)} (${
       perf.audience
     } seats)\n`;
     totalAmount += thisAmount;
