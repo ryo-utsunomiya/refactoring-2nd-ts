7cbede66f0f8229ede86e4e107a403fd918397d4
Ryo Utsunomiya <millionsnowliving@gmail.com>
2018-12-02 20:50:25 +0900

    split loop

--- statement.ts
+++ statement.ts
@@ -65,15 +65,16 @@ export function statement(
   };
 
   for (let perf of invoice.performances) {
-    // add volume credits
-    volumeCredits += volumeCreditsFor(perf);
-
     // print line for this order
     result += `  ${playFor(perf).name}: ${usd(amountFor(perf))} (${
       perf.audience
     } seats)\n`;
     totalAmount += amountFor(perf);
   }
+  for (let perf of invoice.performances) {
+    volumeCredits += volumeCreditsFor(perf);
+  }
+
   result += `Amount owed is ${usd(totalAmount)}\n`;
   result += `You earned ${volumeCredits} credits\n`;
   return result;
