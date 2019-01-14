ae81f2ebc912fbd1fc9140e61712cc53eb2afd15
Ryo Utsunomiya <millionsnowliving@gmail.com>
2019-01-14 17:54:26 +0900

    Add PerformanceCalculator

---
+++ PerformanceCalculator.ts
@@ -0,0 +1,34 @@
+import { Performance, Play } from "./types";
+
+export default class PerformanceCalculator {
+  performance: Performance;
+  play: Play;
+
+  constructor(aPerformance: Performance, aPlay: Play) {
+    this.performance = aPerformance;
+    this.play = aPlay;
+  }
+
+  get amount(): number {
+    let result = 0;
+
+    switch (this.play.type) {
+      case "tragedy":
+        result = 40000;
+        if (this.performance.audience > 30) {
+          result += 1000 * (this.performance.audience - 30);
+        }
+        break;
+      case "comedy":
+        result = 30000;
+        if (this.performance.audience > 20) {
+          result += 10000 + 500 * (this.performance.audience - 20);
+        }
+        result += 300 * this.performance.audience;
+        break;
+      default:
+        throw new Error(`unknown type: ${this.play.type}`);
+    }
+    return result;
+  }
+}
